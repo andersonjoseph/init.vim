7d2de41dac56aac8b40ec3d8804eea1ca38c0668
Anderson Joseph <andersonjoseph@mailfence.com>
2024-04-15 14:18:54 -0400

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,8 @@ return {
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     require("everforest").setup({
-      vim.cmd("colorscheme everforest")
+      background = "hard",
     })
+    vim.cmd("colorscheme everforest")
   end,
 }
