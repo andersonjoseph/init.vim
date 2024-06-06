42336d11f8444aca14fcd306c5b834b3f201c95c
Anderson Joseph <andersonjoseph@mailfence.com>
2024-06-06 13:09:04 -0400

    add osc 52 clipboard

--- lua/settings.lua
+++ lua/settings.lua
@@ -3,6 +3,25 @@ vim.g.guifont = "Cascadia Code:h13"
 
 vim.opt.shiftwidth = 2
 vim.opt.ignorecase = true
-vim.opt.mouse = a
+vim.opt.mouse = ""
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+function no_paste(reg)
+  return function(lines)
+    -- noop
+  end
+end
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  -- https://github.com/neovim/neovim/issues/28611
+  paste = {
+    ['+'] = no_paste("+"), -- Pasting disabled
+    ['*'] = no_paste("/"), -- Pasting disabled
+  },
+}
