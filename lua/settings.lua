26619b05ae40fb0139efc3b5aba4fbf7a16b4dbf
anderson <andersonjoseph@mailfence.com>
2024-06-25 20:29:12 -0400

    remove windows clipboard config

--- lua/settings.lua
+++ lua/settings.lua
@@ -6,22 +6,3 @@ vim.opt.ignorecase = true
 vim.opt.mouse = ""
 vim.opt.number = true
 vim.opt.relativenumber = true
-
-function no_paste(reg)
-  return function(lines)
-    -- noop
-  end
-end
-
-vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-  },
-  -- https://github.com/neovim/neovim/issues/28611
-  paste = {
-    ['+'] = no_paste("+"), -- Pasting disabled
-    ['*'] = no_paste("/"), -- Pasting disabled
-  },
-}
